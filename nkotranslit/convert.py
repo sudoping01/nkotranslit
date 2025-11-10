86e8b350d050709c9cdf9921490855957364afd0
sudoping01 <sudoping01@gmail.com>
2025-11-10 12:31:52 +0000

    add licence (MIT)

--- nkotranslit/convert.py
+++ nkotranslit/convert.py
@@ -2,13 +2,26 @@
 # -*- coding: utf-8 -*-
 
 """
-Created by Tapo, Seydou DIALLO et al. 11-10-2025
+Tapo, Seydou DIALLO et al. 11-10-2025
 
 A self-contained, state-of-the-art Python script for bidirectional,
 rule-based conversion of Bambara text between Latin and N'Ko scripts.
 
 This script uses order-dependent rules to correctly handle digraphs,
 nasal vowels, and other orthographic features.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import sys
