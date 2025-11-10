3bc27b922dadb3d21fffa24a233cf079f87d9c46
sudoping01 <sudoping01@gmail.com>
2025-11-10 15:14:22 +0000

    add gui command

--- nkotranslit/cli.py
+++ nkotranslit/cli.py
@@ -1,4 +1,5 @@
 from nkotranslit.convert import NkoLatinConverter
+from nkotranslit.gui import launch_gui
 
 def latin_to_nko_cli():
     import sys
@@ -11,3 +12,7 @@ def nko_to_latin_cli():
     converter = NkoLatinConverter()
     sentence = " ".join(sys.argv[1:])
     print(converter.convert_text(sentence, converter.NKO_TO_LATIN_RULES))
+
+
+def launch_gui_cli():
+    launch_gui()
