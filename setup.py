c0e92174d51a9af28cb4fba0871973891b90f160
sudoping01 <sudoping01@gmail.com>
2025-11-10 15:16:51 +0000

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="nkotranslit",    
-    version="1.0.1",
+    version="1.1.0",
     description="A bidirectional Latin ↔ N'Ko transliteration library for Bamanankan.",
     long_description=long_description,
     long_description_content_type="text/markdown",
